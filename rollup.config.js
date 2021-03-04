457070ebd3b84791c86e69e4b66f34bffcc5d536
Devin Spikowski <vegeta897@gmail.com>
2021-03-04 06:00:33 -0500

    :wrench: Add dotenv and plugin/replace to handle env variables

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
 import css from 'rollup-plugin-css-only'
 import image from '@rollup/plugin-image'
+import { config } from 'dotenv'
+import replace from '@rollup/plugin-replace'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -42,6 +44,9 @@ export default {
 		file: 'public/build/bundle.js',
 	},
 	plugins: [
+		replace({
+			process: JSON.stringify({ env: { ...config().parsed } }),
+		}),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
